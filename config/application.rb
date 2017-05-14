e77feb5f6b6403487c6605c5d4b3898fc553c602
Leonardo Padilha <lcfpadilha@gmail.com>
2017-05-13 21:31:52 -0300

    adicionando suporte ao CORS

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,20 @@ module HackathonGloboAPI
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+          allow do
+            # origins '*'
+            # resource '*', :headers => :any, :methods => [:get, :post, :options]
+
+            origins '*'
+
+            resource '*',
+                :headers => :any,
+                :methods => [:get, :post, :delete, :put, :options, :head],
+                :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                :max_age => 0
+          end
+    end
   end
 end
